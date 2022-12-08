711f4afcc63337a9de14de2d8034d94d826023fb
memto <dvn0zzz@gmail.com>
2022-12-08 18:41:58 +0700

    8.1: One-to-one unidirectional (part1: files structure)

---
+++ src/main/java/com/nbstech/spring/basic/dataaccess/Relationships/OneToOne/Repository/PlayerProfileRepository.java
@@ -0,0 +1,10 @@
+package com.nbstech.spring.basic.dataaccess.Relationships.OneToOne.Repository;
+
+import com.nbstech.spring.basic.dataaccess.Relationships.OneToOne.Entity.PlayerProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerProfileRepository extends JpaRepository<PlayerProfile, Integer> {
+
+}
