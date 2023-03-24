831f3efe5d51ce1efa0bc70df45a415232477b8b
memto <dvn0zzz@gmail.com>
2023-03-24 16:38:48 +0700

    7.6 - @GetMapping: get all players

--- src/main/java/com/nbstech/spring/basic/dataaccess/restapi/PlayerController.java
+++ src/main/java/com/nbstech/spring/basic/dataaccess/restapi/PlayerController.java
@@ -1,12 +1,23 @@
 package com.nbstech.spring.basic.dataaccess.restapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class PlayerController {
+    @Autowired
+    PlayerService playerService;
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Tennis Player REST API";
+    }
+
+    @GetMapping("/players")
+    public List<PlayerEntity> getAllPlayers() {
+        return playerService.getAllPlayers();
     }
 }
--- src/main/java/com/nbstech/spring/basic/dataaccess/restapi/PlayerService.java
+++ src/main/java/com/nbstech/spring/basic/dataaccess/restapi/PlayerService.java
@@ -3,12 +3,17 @@ package com.nbstech.spring.basic.dataaccess.restapi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PlayerService {
     @Autowired
     PlayerRepository playerRepository;
 
     //Get all players
+    public List<PlayerEntity> getAllPlayers() {
+        return playerRepository.findAll();
+    }
 
     //Get player by ID
 
